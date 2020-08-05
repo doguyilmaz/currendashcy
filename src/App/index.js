6601f70937116d18506daf38f3b1a345e131647c
Kaan Yilmaz <douscriptist@gmail.com>
2020-08-06 01:27:05 +0300

    navbar style & basic implement with grid

--- src/App/index.js
+++ src/App/index.js
@@ -4,10 +4,12 @@ import './App.css';
 import AppLayout from '../Components/Layout/AppLayout';
 import Welcome from '../Components/Welcome';
 import Buttons from '../Components/Buttons';
+import AppBar from '../Components/Layout/AppBar';
 
 function App() {
 	return (
 		<AppLayout>
+			<AppBar />
 			<Welcome name='Doğu' />
 			<Buttons />
 		</AppLayout>
